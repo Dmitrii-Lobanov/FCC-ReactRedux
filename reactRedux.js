52950e565609b5d79448ffd490a2a8971905eb11
Dmitrii Lobanov <41771694+Dmitrii-Lobanov@users.noreply.github.com>
2019-07-15 14:33:16 +0500

    Update reactRedux.js

--- reactRedux.js
+++ reactRedux.js
@@ -40,3 +40,24 @@ class DisplayMessages extends React.Component {
     );
   }
 };
+
+// define ADD, addMessage(), messageReducer(), and store here:
+const ADD = 'ADD';
+
+const addMessage = (message) =>{
+    return {
+        type: ADD, 
+        message
+    };
+};
+
+const messageReducer = (state=[], action) => {
+    switch(action.type){
+        case ADD:
+            return [...state, action.message];
+        default:
+            return state;
+    }
+}
+
+const store = Redux.createStore(messageReducer);
